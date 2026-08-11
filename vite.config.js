a20a5d7fc4a8724f13c40efd038009dcd0a6c8e1
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2026-08-10 20:40:56 -0400

    feat: agregar páginas para build

--- vite.config.js
+++ vite.config.js
@@ -16,4 +16,16 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    rollupOptions: {
+      input: {
+        inicio: fileURLToPath(new URL('./index.html', import.meta.url)),
+        nosotros: fileURLToPath(new URL('./nosotros.html', import.meta.url)),
+        proyectos: fileURLToPath(new URL('./proyectos.html', import.meta.url)),
+        patrimonio: fileURLToPath(new URL('./patrimonio.html', import.meta.url)),
+        escuela: fileURLToPath(new URL('./escuela.html', import.meta.url)),
+        recursos: fileURLToPath(new URL('./recursos.html', import.meta.url))
+      },
+    },
+  },
 });

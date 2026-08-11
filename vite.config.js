fa2a8b5147b9798bdb8b387744bb2d7f58257426
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2026-08-10 20:16:30 -0400

    feat: agregar archivo configuración de vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 // vite.config.js
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        /*  silenceDeprecations: ['import', 'mixed-decls', 'color-functions', 'global-builtin'], */
+        quietDeps: true, // silencia warnings que vienen de node_modules (Bootstrap, etc.)
+      },
     },
   },
 });
